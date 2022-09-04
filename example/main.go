a18d2200bcf3bc936e7e2622a3815e6d16542b87
Jason Cochard <cochard@protonmail.com>
2022-09-04 15:03:18 -0700

    initial example

---
+++ example/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"machine"
+	"time"
+
+	trig "github.com/eyelight/trigger"
+)
+
+var (
+	btn        = machine.D3
+	dispatchCh = make(chan trig.Trigger, 1)
+	fakeMqttCh = make(chan trig.Trigger, 1)
+)
+
+func main() {
+	light1 := NewPeripheral("LightBulb1")
+	light2 := NewPeripheral("LightBulb2")
+	dispatch := trig.NewDispatch(dispatchCh)
+	dispatch.AddToDispatch(&light1, &light2)
+	misoMqtt := NewFakeMqtt(fakeMqttCh)
+	go dispatch.Dispatch()
+	go misoMqtt.ListenAndSend()
+
+	triggerFromMqttA := trig.Trigger{
+		Target:   "LightBulb1",
+		Action:   "FakeOn",
+		Duration: time.Duration(5 * time.Second),
+		Report:   true,
+		ReportCh: fakeMqttCh,
+	}
+	triggerFromMqttB := trig.Trigger{
+		Target:   "LightBulb2",
+		Action:   "FakeOff",
+		Duration: time.Duration(0),
+		Report:   true,
+		ReportCh: fakeMqttCh,
+	}
+	triggerFromMqttC := trig.Trigger{
+		Target:   "LightBulb3",
+		Action:   "FakeToggle",
+		Duration: time.Duration(0),
+		Report:   true,
+		ReportCh: fakeMqttCh,
+	}
+	triggerFromMqttD := trig.Trigger{
+		Target:   "LightBulb1",
+		Action:   "FakeOn",
+		Duration: time.Duration(0),
+		Report:   true,
+		ReportCh: fakeMqttCh,
+	}
+
+	dispatchCh <- triggerFromMqttA
+	time.Sleep(2 * time.Second)
+	dispatchCh <- triggerFromMqttB
+	time.Sleep(1 * time.Second)
+	dispatchCh <- triggerFromMqttC
+	time.Sleep(200 * time.Millisecond)
+	dispatchCh <- triggerFromMqttD
+	select {}
+}
+
+type responder struct {
+	name string
+}
+
+func NewPeripheral(n string) responder {
+	return responder{
+		name: n,
+	}
+}
+
+func (r *responder) Name() string {
+	return r.name
+}
+
+// func (r *responder) Trigger() chan trig.Trigger {
+// 	return r.ch
+// }
+
+func (r *responder) Execute(t trig.Trigger) {
+	if t.Target != r.name {
+		t.Error, t.Report = true, false
+		t.Message = string("error - " + r.name + " received a trigger intended for " + t.Target)
+		t.ReportCh <- t
+		return
+	}
+	switch t.Action {
+	case "FakeOn":
+		println("FakeOn")
+		t.Error, t.Report = false, false
+		t.Message = string(r.name + " executing FakeOn at " + time.Now().String())
+		if t.Duration > 0 {
+			go func() {
+				println("anon")
+				time.Sleep(t.Duration)
+				t.Message = string(r.name + " executed FakeOff after " + t.Duration.String())
+				t.ReportCh <- t
+			}()
+		}
+		t.ReportCh <- t
+	case "FakeOff":
+		println("FakeOff")
+		t.Error, t.Report = false, false
+		t.Message = string(r.name + " executing FakeOff at " + time.Now().String())
+		t.ReportCh <- t
+	case "FakeToggle":
+		println("FakeToggle")
+		t.Error, t.Report = false, false
+		t.Message = string(r.name + " executing FakeToggle at " + time.Now().String())
+		t.ReportCh <- t
+	}
+}
+
+type fakeMqtt struct {
+	input chan trig.Trigger
+}
+
+func NewFakeMqtt(ch chan trig.Trigger) fakeMqtt {
+	return fakeMqtt{
+		input: ch,
+	}
+}
+
+func (m *fakeMqtt) ListenAndSend() {
+	for {
+		select {
+		case t := <-m.input:
+			println("Mqtt MISO Reponse: " + t.Message)
+		}
+	}
+}
